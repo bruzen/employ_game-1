7ac2f6d4e21db3c6d0664b4bdc0833489f5f923f
Terry Stewart <tcstewar@uwaterloo.ca>
2014-08-27 14:18:11 -0400

    Working overview display

--- employ_game/server.py
+++ employ_game/server.py
@@ -1,5 +1,6 @@
 import os.path
 import json
+import random
 
 import pkgutil
 import employ_game
@@ -31,6 +32,47 @@ class Server(employ_game.swi.SimpleWebInterface):
             return self.create_login_form()
         html = pkgutil.get_data('employ_game', 'templates/index.html')
         return html
+        
+    def swi_overview(self):
+        if self.user is None:
+            return self.create_login_form()
+        html = pkgutil.get_data('employ_game', 'templates/overview.html')
+        return html
+        
+    def swi_overview_json(self):
+        maximum = 10
+        users = 5
+        substeps = 20
+        steps = random.randrange(1, maximum) * substeps
+        #steps = 3 * substeps
+        
+        time = []
+        for i in range(users):
+            color = ['blue', 'red', 'green', 'magenta', 'cyan', 'black'][i % 6]
+            key = 'User%d' % i
+            values = [dict(x=0, y=0)]
+            for j in range(1, steps):
+                y2 = values[-1]['y'] + random.gauss(0, 5)
+                values.append(dict(x=float(j)/substeps, y=y2))
+            values.append(dict(x=maximum, y=None))
+            time.append(dict(values=values, key=key, color=color, area=False))    
+            
+        bar = []
+        for i in range(users):
+            color = ['blue', 'red', 'green', 'magenta', 'cyan', 'black'][i % 6]
+            key = 'User%d' % i
+            values = [dict(x=0, y=random.gauss(1, 0.5)), dict(x=1, y=random.gauss(2, 0.7)), dict(x=2, y=random.gauss(0, 0.3))]
+            bar.append(dict(values=values, key=key, color=color))    
+            
+    
+        return json.dumps(dict(time=time, bar=bar))
+
+    def swi_play(self):
+        if self.user is None:
+            return self.create_login_form()
+        html = pkgutil.get_data('employ_game', 'templates/play.html')
+        return html
+        
 
     def create_login_form(self):
         message = "Enter the password:"
