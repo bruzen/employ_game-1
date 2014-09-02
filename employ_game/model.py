961399344c1261f4e3c099d428d3be7a55e054e7
Terry Stewart <tcstewar@uwaterloo.ca>
2014-09-02 13:04:30 -0400

    Basic jobs

--- employ_game/model.py
+++ employ_game/model.py
@@ -14,8 +14,19 @@ class Society:
         self.probs = [
             ('prison', dict(overall=0.4, male=0.2, female=0.1, white=0.2,
                            black=0.3, black_male=0.9)),
+            ('highschool', dict(overall=0.8, male=0.8, female=0.8, white=0.9,
+                           black=0.7)),
 
             ]
+
+        self.jobs = {
+            'service': dict(highschool=0.5),
+            'security': dict(highschool=0.7, prison=None),
+            }
+        self.job_commonality = {
+            'service': 0.5,
+            'security': 0.5,
+            }
 
     def create_features(self):
         features = []
@@ -25,6 +36,8 @@ class Society:
             p = self.compute_conditional(features, prob)
             if self.rng.rand() < p:
                 features.append(feature)
+            else:
+                features.append('no_' + feature)
         return features
 
     def pick_one(self, options):
@@ -55,17 +68,36 @@ class Society:
         return probability
 
 
-
-
-
-
 class Person:
     def __init__(self, society):
         self.rng = rng
         self.age = 16
         self.job = None
         self.features = society.create_features()
+        self.attributes = {}
 
+class Employer:
+    jobs_per_employer = 10
+    def __init__(self, society):
+        self.jobs = [Job(society) for i in range(self.jobs_per_employer)]
+
+class Job:
+    def __init__(self, society):
+        self.employee = None
+        self.society = society
+        self.type = society.rng.choice(society.job_commonality.keys(),
+                                       p=society.job_commonality.values())
+
+    def compute_suitability(self, person):
+        total = 0
+        for feature, value in self.society.jobs[self.type].items():
+            if value is None and feature in person.features:
+                return None
+            if feature in person.features:
+                total += value
+            elif feature in person.attributes:
+                total += value * person.attributes[feature]
+        return total
 
 
 
@@ -74,7 +106,11 @@ if __name__ == '__main__':
     rng = np.random.RandomState()
     society = Society(rng)
     people = [Person(society) for i in range(10)]
+    job = Job(society)
+    print job.type
     for p in people:
-        print p.features
+        print p.features, job.compute_suitability(p)
+
+
 
 
