4944e34673dbc73a9a7a2a56c360ff7a6a838565
Terry Stewart <tcstewar@uwaterloo.ca>
2014-09-02 14:44:25 -0400

    Simple hiring process

--- employ_game/model.py
+++ employ_game/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 class Society:
+    neighbourhood_count = 3
     def __init__(self, rng):
         self.rng = rng
         self.gender = dict(male=0.5, female=0.5)
@@ -27,6 +28,21 @@ class Society:
             'service': 0.5,
             'security': 0.5,
             }
+        self.job_income = {
+            'service': (20000, 1000),   # starting, annual raise
+            'security': (40000, 2000),
+        }
+
+        self.neighbourhoods = [Neighbourhood(self)
+                               for i in range(self.neighbourhood_count)]
+
+    def create_attributes(self):
+        attr = {}
+        attr['prob_apply'] = self.rng.normal(0.5, 0.25)
+        attr['distance_penalty'] = self.rng.uniform(0, 0.5)
+        attr['interview_skill'] = self.rng.normal(0.2, 0.2)
+        attr['interview_skill_sd'] = self.rng.uniform(0.1, 0.4)
+        return attr
 
     def create_features(self):
         features = []
@@ -70,20 +86,39 @@ class Society:
 
 class Person:
     def __init__(self, society):
-        self.rng = rng
+        self.society = society
         self.age = 16
         self.job = None
         self.features = society.create_features()
-        self.attributes = {}
+        self.attributes = society.create_attributes()
+        self.neighbourhood = society.rng.choice(society.neighbourhoods)
+
+    def does_apply(self, job):
+        p = self.attributes['prob_apply']
+        if job.employer.neighbourhood is not self.neighbourhood:
+            p -= self.attributes['distance_penalty']
+        return self.society.rng.rand() < p
+
+    def compute_suitability(self, job):
+        score = 0
+        if self.neighbourhood is job.employer.neighbourhood:
+            score += 5000
+        score += self.society.job_income[job.type][0]
+        return score
+
+
+
 
 class Employer:
     jobs_per_employer = 10
     def __init__(self, society):
-        self.jobs = [Job(society) for i in range(self.jobs_per_employer)]
+        self.jobs = [Job(society, self) for i in range(self.jobs_per_employer)]
+        self.neighbourhood = society.rng.choice(society.neighbourhoods)
 
 class Job:
-    def __init__(self, society):
+    def __init__(self, society, employer):
         self.employee = None
+        self.employer = employer
         self.society = society
         self.type = society.rng.choice(society.job_commonality.keys(),
                                        p=society.job_commonality.values())
@@ -92,24 +127,100 @@ class Job:
         total = 0
         for feature, value in self.society.jobs[self.type].items():
             if value is None and feature in person.features:
-                return None
+                return -np.inf
             if feature in person.features:
                 total += value
             elif feature in person.attributes:
                 total += value * person.attributes[feature]
         return total
 
+class Neighbourhood:
+    def __init__(self, society):
+        self.society = society
+
+
+class Model:
+    employer_count = 100
+    people_per_step = 10
+    years_per_step = 0.1
+    max_age = 25
+
+    def __init__(self, seed=None):
+        self.rng = np.random.RandomState(seed=seed)
+        self.society = Society(self.rng)
+        self.employers = [Employer(self.society)
+                          for i in range(self.employer_count)]
+        self.people = []
+
+    def step(self):
+        for i in range(self.people_per_step):
+            self.people.append(Person(self.society))
+
+        applications = {}
+        for e in self.employers:
+            for j in e.jobs:
+                if j.employee is None:
+                    applications[j] = []
+        for p in self.people:
+            if p.job is None:
+                for j in applications.keys():
+                    if p.does_apply(j):
+                        applications[j].append(p)
+
+        iterations = 10
+        for i in range(iterations):
+            all_offers = {}
+            for job, applicants in applications.items():
+
+                #TODO: optimize this so it isn't computed each iteration
+                score = [job.compute_suitability(a) for a in applicants]
+
+                for i,a in enumerate(applicants):
+                    score[i] += self.rng.normal(a.attributes['interview_skill'],
+                                                a.attributes['interview_skill_sd'])
+
+                if len(score) > 0:
+                    max_score = max(score)
+                    if max_score > 0:
+                        index = score.index(max_score)
+                        person = applicants[index]
+                        if person not in all_offers:
+                            all_offers[person] = []
+                        all_offers[person].append(job)
+
+            for person, offers in all_offers.items():
+                score = [person.compute_suitability(j) for j in offers]
+                if len(score) > 0:
+                    max_score = max(score)
+                    if max_score > 0:
+                        index = score.index(max_score)
+                        job = offers[index]
+
+                        person.job = job
+                        job.employee = person
+
+    def calc_employment(self):
+        count = 0
+        for p in self.people:
+            if p.job is not None:
+                count += 1
+        return float(count)/len(self.people)
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
-    rng = np.random.RandomState()
-    society = Society(rng)
-    people = [Person(society) for i in range(10)]
-    job = Job(society)
-    print job.type
-    for p in people:
-        print p.features, job.compute_suitability(p)
+
+    m = Model()
+    m.step()
+    for i in range(100):
+        print i, len(m.people), m.calc_employment()
+        m.step()
 
 
 
